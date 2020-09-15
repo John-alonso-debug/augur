31bc2907296e807919c596fd50a5dd4cb3410c41
robert <robert.davidson@forecastfoundation.org>
2020-09-14 22:24:43 -0700

    amm test incl. factory. WIP

--- packages/augur-core/tests/test_amm.py
+++ packages/augur-core/tests/test_amm.py
@@ -11,15 +11,25 @@ def test_amm(contractsFixture, market, cash):
 
     shareToken = contractsFixture.getShareToken()
 
+    # Create factory
+    cloneableAMM = contractsFixture.upload('../src/contracts/para/AMMExchange.sol')
+    factory = contractsFixture.upload('../src/contracts/para/AMMFactory.sol', constructorArgs=[cloneableAMM.address])
+
     # Create AMM
-    amm = contractsFixture.upload('../src/contracts/para/AMMExchange.sol')
-    amm.initialize(market.address, shareToken.address)
+    fee = 3 # 3/1000
+    ammAddress = factory.addAMM(market.address, shareToken.address, fee)
+    amm = contractsFixture.applySignature("AMMExchange", ammAddress)
+
+    print("MARINA", market.getNumTicks())
 
     # Add liquidity
-    cash.faucet(100000 * 10**18)
-    cash.approve(amm.address, 10**40)
-    amm.addLiquidity(10 * 10**18)
+    cash.faucet(100000e18)
+    cash.approve(amm.address, 1e48)
+    amm.addLiquidity(100e18) # this dies at 100e18 but succeeds at 99e18
+#     amm.removeLiquidity(50e18)
 
     # Enter position
+
+#     amm.enterPosition(1e18, True, 1000e18)
 
     # Exit position
